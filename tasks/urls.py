15f1cb6419e11a2b512b9916dd6cf36b035a6e91
Biltin108010 <Villanueva.johnrhey4@gmail.com>
2025-04-03 21:55:54 +0800

    help

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin 
 from django.urls import path
 from .views import AddTaskView, EditTaskView, DeleteTaskView, SearchTaskView, TaskDetailView
 from django.http import HttpResponse
