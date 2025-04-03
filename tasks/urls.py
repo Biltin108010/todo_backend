95082aa0334f1f3e238d6ee7dee9ddf66e5b73b7
Biltin108010 <Villanueva.johnrhey4@gmail.com>
2025-04-03 21:49:08 +0800

    adjustments

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import AddTaskView, EditTaskView, DeleteTaskView, SearchTaskView, TaskDetailView
 from django.http import HttpResponse
 
+# Your home view
 def home(request):
     return HttpResponse("Welcome to the Todo app!")
 
 urlpatterns = [
+    path('', home, name='home'),  # This will handle requests to the root URL '/'
     path('tasks/add/', AddTaskView.as_view(), name='task-add'),
     path('tasks/edit/<int:pk>/', EditTaskView.as_view(), name='task-edit'),
     path('tasks/delete/<int:pk>/', DeleteTaskView.as_view(), name='task-delete'),
